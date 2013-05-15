magomar/ares

commit 6caa598e0cdbe0f9967341e1d28a4587aa613d20

Paint method for AbstractImageLayer and its derived classes is limited to the viewport area. This change noticeably improves scrolling in big scenarios

src/ares/application/gui/AbstractImageLayer.java
@@ -2,6 +2,7 @@ package ares.application.gui;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import javax.swing.JViewport;
 
 /**
  *
@@ -14,22 +15,33 @@ public abstract class AbstractImageLayer extends javax.swing.JPanel implements I
      */
     protected BufferedImage globalImage;
     /**
-     * A layer that shares his {@link #globalImage} with this layer This
-     * attribute is null if the layer uses its own {@link #globalImage}
+     * A layer that shares his {@link #globalImage} with this layer This attribute is null if the layer uses its own
+     * {@link #globalImage}
      */
     protected AbstractImageLayer parentLayer;
+    protected JViewport viewport;
 
     /**
-     * Creates a new instance of this class with independent graphics
-     * ({@code parentLayer == null})
+     * Creates a new instance of this class with independent graphics ({@code parentLayer == null})
      */
-    public AbstractImageLayer() {
+    public AbstractImageLayer(JViewport viewport) {
+        this(viewport, null);
+    }
+
+//    /**
+//     * Creates a new instance of this class with independent graphics ({@code parentLayer == null})
+//     */
+//    public AbstractImageLayer() {
+//        setOpaque(false);
+//    }
+    public AbstractImageLayer(JViewport viewport, AbstractImageLayer parentLayer) {
         setOpaque(false);
+        this.viewport = viewport;
+        this.parentLayer = parentLayer;
     }
 
     /**
-     * Creates a new instance of this class sharing its graphics with
-     * {@code parentLayer}
+     * Creates a new instance of this class sharing its graphics with {@code parentLayer}
      */
     public AbstractImageLayer(AbstractImageLayer parentLayer) {
         setOpaque(false);
@@ -48,7 +60,6 @@ public abstract class AbstractImageLayer extends javax.swing.JPanel implements I
 
 //    @Override
 //    public abstract void updateLayer();
-
     @Override
     public final void flush() {
         globalImage = null;
@@ -67,12 +78,15 @@ public abstract class AbstractImageLayer extends javax.swing.JPanel implements I
         }
         Graphics2D g2 = (Graphics2D) g;
         if (globalImage != null) {
-            g2.drawImage(globalImage, 0, 0, this);
+            if (viewport != null) {
+                Rectangle rect = viewport.getViewRect();
+                BufferedImage viewImage = globalImage.getSubimage(rect.x, rect.y, rect.width, rect.height);
+                g2.drawImage(viewImage, rect.x, rect.y, this);
+            } else {
+                g2.drawImage(globalImage, 0, 0, this);
+            }
+
         }
-//        else {
-//            g2.setBackground(Color.BLACK);
-//            g2.fillRect(0, 0, this.getWidth(), this.getHeight());
-//        }
     }
 
     @Override
@@ -84,6 +98,4 @@ public abstract class AbstractImageLayer extends javax.swing.JPanel implements I
     public BufferedImage getGlobalImage() {
         return globalImage;
     }
-    
-    
 }

src/ares/application/gui/board/GridLayer.java
@@ -8,6 +8,7 @@ import ares.application.models.ScenarioModel;
 import ares.application.models.board.TileModel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import javax.swing.JViewport;
 
 /**
  * Grid image layer
@@ -17,6 +18,10 @@ import java.awt.image.BufferedImage;
 public class GridLayer extends AbstractImageLayer {
 
     private ScenarioModel scenario;
+
+    public GridLayer(JViewport viewport) {
+        super(viewport);
+    }
 
     @Override
     public void updateLayer() {

src/ares/application/gui/board/TerrainLayer.java
@@ -12,6 +12,7 @@ import ares.scenario.board.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.*;
+import javax.swing.JViewport;
 
 /**
  * Terrain image layer based on Sergio Musoles TerrainPanel
@@ -21,6 +22,10 @@ import java.util.*;
 public class TerrainLayer extends AbstractImageLayer {
 
     private ScenarioModel scenario;
+
+    public TerrainLayer(JViewport viewport) {
+        super(viewport);
+    }
 
     @Override
     public void updateLayer() {

src/ares/application/gui/command/ArrowLayer.java
@@ -11,6 +11,7 @@ import ares.scenario.board.Tile;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.*;
+import javax.swing.JViewport;
 
 /**
  * Draws the movement arrows on a BufferedImage
@@ -25,8 +26,8 @@ public class ArrowLayer extends AbstractImageLayer {
     private Collection<Path> formationPaths;
     private Collection<Path> forcePaths;
 
-    public ArrowLayer(AbstractImageLayer parentLayer) {
-        super(parentLayer);
+    public ArrowLayer(JViewport viewport, AbstractImageLayer parentLayer) {
+        super(viewport, parentLayer);
     }
 
     @Override
@@ -177,8 +178,7 @@ public class ArrowLayer extends AbstractImageLayer {
      * Paints a single arrow segment together with the accumulated movement cost
      *
      * @param tile the tile where to paint an Arrow
-     * @param index the position of the arrow segment within the array of arrow
-     * images
+     * @param index the position of the arrow segment within the array of arrow images
      */
     private void paintArrowSegmentWithCost(Graphics2D g2, Node node, Set<Direction> directions, ArrowType type) {
         Point coordinates = Directions.getDirections(Direction.getBitmask(directions)).getCoordinates();

src/ares/application/gui/command/PathSearchLayer.java
@@ -2,14 +2,13 @@ package ares.application.gui.command;
 
 import ares.application.gui.AbstractImageLayer;
 import ares.application.gui.GraphicsModel;
-import ares.application.gui.providers.AresMiscGraphics;
 import ares.application.io.AresIO;
-import ares.engine.algorithms.pathfinding.Path;
 import ares.scenario.board.Tile;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.Collection;
+import javax.swing.JViewport;
 
 /**
  *Draws the pathfinding process on a BufferedImage
@@ -21,8 +20,8 @@ public class PathSearchLayer extends AbstractImageLayer {
     private Collection<Tile> closedSet;
     private Graphics2D g2;
     
-    public PathSearchLayer(AbstractImageLayer parentLayer) {
-        super(parentLayer);
+    public PathSearchLayer(JViewport viewport, AbstractImageLayer parentLayer) {
+        super(viewport, parentLayer);
     }
     
     @Override

src/ares/application/gui/command/SelectionLayer.java
@@ -2,7 +2,6 @@ package ares.application.gui.command;
 
 import ares.application.gui.GraphicsModel;
 import ares.application.gui.AbstractImageLayer;
-import ares.application.gui.AresGraphicsProfile;
 import ares.application.gui.providers.AresMiscGraphics;
 import ares.application.models.board.*;
 import ares.application.models.forces.FormationModel;
@@ -10,6 +9,7 @@ import ares.application.models.forces.UnitModel;
 import ares.application.io.AresIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import javax.swing.JViewport;
 
 /**
  * Draws the movement arrows on a BufferedImage
@@ -22,6 +22,10 @@ public class SelectionLayer extends AbstractImageLayer {
     private final AresMiscGraphics steelCursor = AresMiscGraphics.STEEL_CURSOR;
     private UnitModel selectedUnit;
     private FormationModel formation;
+
+    public SelectionLayer(JViewport viewport) {
+        super(viewport);
+    }
 
     @Override
     public void updateLayer() {

src/ares/application/gui/forces/UnitsLayer.java
@@ -10,6 +10,7 @@ import ares.application.io.AresIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.*;
+import javax.swing.JViewport;
 
 /**
  * Units image layer based on Sergio Musoles TerrainPanel
@@ -20,7 +21,10 @@ public class UnitsLayer extends AbstractImageLayer {
 
     private ScenarioModel scenario;
 
-//    private TileModel tile;
+    public UnitsLayer(JViewport viewport) {
+        super(viewport);
+    }
+    
     @Override
     public void updateLayer() {
         initialize();

src/ares/application/gui/menu/WelcomeScreen.java
@@ -27,6 +27,7 @@ public final class WelcomeScreen extends AbstractImageLayer {
     private final static File wallpapers = new File(ResourcePath.GRAPHICS.getPath(), "Background");
 
     public WelcomeScreen() {
+        super(null, null);
         BoxLayout bl = new BoxLayout(this, BoxLayout.PAGE_AXIS);
         setLayout(bl);
         add(Box.createRigidArea(new Dimension(0, 300)));

commit e623e51924000d3cec5359cfbfd9bf43948130c2

Solves bug (introduced in commit 6caa598) happening when the board images are smaller than the viewport

src/ares/application/gui/AbstractImageLayer.java
@@ -39,7 +39,6 @@ public abstract class AbstractImageLayer extends JPanel implements ImageLayer {
         this.parentLayer = parentLayer;
     }
 
-
     @Override
     public final void initialize() {
         if (parentLayer == null) {
@@ -49,10 +48,10 @@ public abstract class AbstractImageLayer extends JPanel implements ImageLayer {
         }
         repaint();
     }
+//
+//    @Override
+//    public abstract void updateLayer();
 
-    @Override
-    public abstract void updateLayer();
-    
     @Override
     public final void flush() {
         globalImage = null;
@@ -71,14 +70,12 @@ public abstract class AbstractImageLayer extends JPanel implements ImageLayer {
         }
         Graphics2D g2 = (Graphics2D) g;
         if (globalImage != null) {
-            if (viewport != null) {
-                Rectangle rect = viewport.getViewRect();
-                BufferedImage viewImage = globalImage.getSubimage(rect.x, rect.y, rect.width, rect.height);
-                g2.drawImage(viewImage, rect.x, rect.y, this);
-            } else {
-                g2.drawImage(globalImage, 0, 0, this);
-            }
-
+            Rectangle rect = viewport.getViewRect();
+            int visibleImageWidth = Math.min(rect.width, globalImage.getWidth());
+            int visibleImageHeight = Math.min(rect.height, globalImage.getHeight());
+            BufferedImage viewImage = globalImage.getSubimage(rect.x, rect.y, visibleImageWidth, visibleImageHeight);
+//            BufferedImage viewImage = globalImage.getSubimage(rect.x, rect.y, rect.width, rect.height);
+            g2.drawImage(viewImage, rect.x, rect.y, this);
         }
     }
 