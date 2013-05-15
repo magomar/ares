magomar/ares

commit 2af741260581692976beb5e1ae665aa5588df4a0

Solves a problem rendering text and changes the way units are overlapped when stacked

src/ares/application/gui/AresGraphicsProfile.java
@@ -10,20 +10,20 @@ import ares.platform.io.ResourcePath;
  */
 public enum AresGraphicsProfile implements GraphicsProfile {
 
-    // ResourcePath, Units (Width,Height), Terrain(W,H), Hex(Side,Offset), Font size, Led vert. size, Bar vert. size
-    SMALL(ResourcePath.GRAPHICS_SMALL, 272, 128, 216, 192, 13, 21, 5, 2, 5) {
+    // ResourcePath, Units (Width,Height), Terrain(W,H), Hex(Side,Offset), Unit image offset, unit stack offset, max unit stack, Font size, Led vert. size, Bar vert. size
+    SMALL(ResourcePath.GRAPHICS_SMALL, 272, 128, 216, 192, 13, 21, 3, 0, 0, 5, 2, 5) {
         @Override
         public String getFilename(String filename) {
             return "s_" + filename;
         }
     },
-    MEDIUM(ResourcePath.GRAPHICS_MEDIUM, 496, 248, 408, 352, 28, 39, 10, 4, 10) {
+    MEDIUM(ResourcePath.GRAPHICS_MEDIUM, 496, 248, 408, 352, 28, 39, 6, 2, 3, 10, 4, 10) {
         @Override
         public String getFilename(String filename) {
             return "m_" + filename;
         }
     },
-    HIGH(ResourcePath.GRAPHICS_HIGH, 992, 496, 816, 704, 51, 78, 20, 8, 20) {
+    HIGH(ResourcePath.GRAPHICS_HIGH, 992, 496, 816, 704, 51, 78, 12, 4, 5, 20, 8, 20) {
         @Override
         public String getFilename(String filename) {
             return "h_" + filename;
@@ -35,7 +35,7 @@ public enum AresGraphicsProfile implements GraphicsProfile {
     public final static int TERRAIN_IMAGE_COLS = 8;
     private final int unitsImageWidth;
     private final int unitsImageHeight;
-    private final int unitWitdh;
+    private final int unitWidth;
     private final int unitHeight;
     private final int terrainImageWidth;
     private final int terrainImageHeight;
@@ -46,15 +46,18 @@ public enum AresGraphicsProfile implements GraphicsProfile {
     private final int fontSize;
     private final int ledSize;
     private final int barSize;
+    private final int unitImageOffset;
+    private final int unitStackOffset;
+    private final int maxUnitsStack;
     private final double hexRise = 1.833;
     private final String path;
     private final UnitsInfographicProfile unitsProfile;
 
+
     /**
-     *
-     * AresGraphicsProfile stores information concerning the graphics used in the game i.e: units and terrain image
-     * files and hexagon measures based on the desired profile (SMALL, MEDIUM or HIGH)
-     *
+     * Holds information concerning the graphics used in the game, including the files containing the graphics and 
+     * metrics relative to those graphics (image sizes, distances between reference points (offsets), font size,etc)
+     * 
      * @param resourcePath
      * @param unitsImageWidth
      * @param unitsImageHeight
@@ -62,14 +65,17 @@ public enum AresGraphicsProfile implements GraphicsProfile {
      * @param terrainImageHeight
      * @param hexSide
      * @param hexOffset
+     * @param unitImageOffset
+     * @param unitStackOffset
+     * @param maxUnitsStack
      * @param fontSize
      * @param ledSize
-     * @param barSize
+     * @param barSize 
      */
-    private AresGraphicsProfile(final ResourcePath resourcePath,
-            final int unitsImageWidth, final int unitsImageHeight,
-            final int terrainImageWidth, final int terrainImageHeight,
-            final int hexSide, final int hexOffset, final int fontSize, final int ledSize, final int barSize) {
+    private AresGraphicsProfile(final ResourcePath resourcePath, final int unitsImageWidth, final int unitsImageHeight,
+            final int terrainImageWidth, final int terrainImageHeight, final int hexSide, final int hexOffset,
+            final int unitImageOffset, final int unitStackOffset, final int maxUnitsStack,
+            final int fontSize, final int ledSize, final int barSize) {
         this.path = resourcePath.getPath();
         this.unitsImageWidth = unitsImageWidth;
         this.unitsImageHeight = unitsImageHeight;
@@ -80,7 +86,10 @@ public enum AresGraphicsProfile implements GraphicsProfile {
         this.fontSize = fontSize;
         this.ledSize = ledSize;
         this.barSize = barSize;
-        unitWitdh = unitsImageWidth / UNITS_IMAGE_COLS;
+        this.unitImageOffset = unitImageOffset;
+        this.unitStackOffset = unitStackOffset;
+        this.maxUnitsStack = maxUnitsStack;
+        unitWidth = unitsImageWidth / UNITS_IMAGE_COLS;
         unitHeight = unitsImageHeight / UNITS_IMAGE_ROWS;
         hexDiameter = terrainImageWidth / TERRAIN_IMAGE_COLS;
         hexHeight = terrainImageHeight / TERRAIN_IMAGE_ROWS;
@@ -114,7 +123,7 @@ public enum AresGraphicsProfile implements GraphicsProfile {
      */
     @Override
     public int getUnitWidth() {
-        return unitWitdh;
+        return unitWidth;
     }
 
     /**
@@ -240,5 +249,33 @@ public enum AresGraphicsProfile implements GraphicsProfile {
     public UnitsInfographicProfile getUnitsProfile() {
         return unitsProfile;
     }
-    
+
+    /**
+     * @return the offset distance from the left and upper corners of the tile(same distance vertically and
+     * horizontally). The image will be painted at Point(X+offset, Y+offset)
+     */
+    @Override
+    public int getUnitImageOffset() {
+        return unitImageOffset;
+    }
+
+    /**
+     * A stack of units is represented showing overlapping unit images. Each layer is shifted this number of pixels. For
+     * example, say the first unit was painted at Point(X,Y), then the next layer will start at
+     * Point(X+offset,Y+offset), and the third one at Point(X+2*offset, Y+2*offset) and so on.
+     *
+     * @return the offset distance both horizontally and vertically between unit images painted in the same tile.
+     */
+    @Override
+    public int getUnitStackOffset() {
+        return unitStackOffset;
+    }
+
+    /**
+     * @return the maximum numbers of units to be painted in a single tile
+     */
+    @Override
+    public int getMaxUnitsStack() {
+        return maxUnitsStack;
+    }
 }

src/ares/application/gui/forces/UnitsLayer.java
@@ -2,18 +2,12 @@ package ares.application.gui.forces;
 
 import ares.application.gui.GraphicsModel;
 import ares.application.gui.AbstractImageLayer;
+import ares.application.gui.GraphicsProfile;
 import ares.application.models.ScenarioModel;
 import ares.application.models.board.*;
 import ares.application.models.forces.*;
-import ares.engine.knowledge.KnowledgeCategory;
 import ares.application.io.AresIO;
-import static ares.engine.knowledge.KnowledgeCategory.COMPLETE;
-import static ares.engine.knowledge.KnowledgeCategory.GOOD;
-import static ares.engine.knowledge.KnowledgeCategory.NONE;
-import static ares.engine.knowledge.KnowledgeCategory.POOR;
-import ares.scenario.forces.Echelon;
 import java.awt.*;
-import java.awt.color.ICC_ProfileRGB;
 import java.awt.image.BufferedImage;
 import java.util.*;
 
@@ -25,29 +19,13 @@ import java.util.*;
 public class UnitsLayer extends AbstractImageLayer {
 
     private ScenarioModel scenario;
-    /**
-     * Offset distance from the upper left corner of the tile. The image will be painted at Point(X+offset, Y+offset)
-     *
-     */
-    private final static int UNIT_IMAGE_OFFSET = 7;
-    /**
-     * Maximum numbers of units to be painted in a single tile
-     *
-     */
-    private final static int MAX_STACK = 6;
-    /**
-     * The unit image can be composed of different layered images. Each layer is shifted {@code unitStackOffset} pixels.
-     * For example, say the first layer was painted at Point(X,Y), then the next layer will start at
-     * Point(X+offset,Y+offset), and the third one at Point(X+2*offset, Y+2*offset) and so on.
-     *
-     */
-    private static int UNIT_STACK_OFFSET = 1;
 
 //    private TileModel tile;
     @Override
     public void updateLayer() {
         initialize();
         Graphics2D g2 = globalImage.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Collection<TileModel> tileModels = new HashSet<>();
         for (ForceModel forceModel : scenario.getForceModel()) {
             for (UnitModel unitModel : forceModel.getUnitModels()) {
@@ -89,40 +67,43 @@ public class UnitsLayer extends AbstractImageLayer {
      */
     private void paintUnitStack(Graphics2D g2, TileModel tile) {
 
-        //Calculate unit position
-        Point pos = GraphicsModel.INSTANCE.tileToPixel(tile.getCoordinates());
-
         if (!tile.isEmpty()) {
+            //Calculate unit location in board
+            Point pos = GraphicsModel.INSTANCE.tileToPixel(tile.getCoordinates());
             //Retrieve the single unit image
             UnitModel unit = tile.getTopUnit();
             BufferedImage unitImage = GraphicsModel.INSTANCE.getActiveProvider(unit.getColor()).getImage(unit.getIconId(), AresIO.ARES_IO);
-
+            GraphicsProfile graphicsProfile = GraphicsModel.INSTANCE.getActiveProfile();
             //Num units to be painted
-            int max = Math.min(tile.getNumStackedUnits(), MAX_STACK);
+            int unitsToPaint = Math.min(tile.getNumStackedUnits(), graphicsProfile.getMaxUnitsStack());
             // Attributes are painted only on the last image
-            max--;
+            unitsToPaint--;
 
             // Offset from the upper left corner of the tile
-            pos.x += UNIT_IMAGE_OFFSET;
-            pos.y += UNIT_IMAGE_OFFSET;
+            int imageOffset = graphicsProfile.getUnitImageOffset();
+            pos.x += imageOffset;
+            pos.y += imageOffset;
 
+            int stackOffset = graphicsProfile.getUnitStackOffset();
             // Offset from the upper left corner of the last painted unit
-            // incremented by unitStackOffset
-            int d = 0;
+            int maxOffset = unitsToPaint * stackOffset;
 
             //Paint the same top unit
-            for (int i = 0; i < max; i++) {
+            for (int d = maxOffset; d >= 0; d -= stackOffset) {
                 g2.drawImage(unitImage, pos.x + d, pos.y + d, this);
-                d += UNIT_STACK_OFFSET;
             }
+            BufferedImage subImage = globalImage.getSubimage(pos.x, pos.y, graphicsProfile.getUnitWidth(), graphicsProfile.getUnitHeight());
 
-            //Adds attributes to the image such as Health, Attack, Defense, etc.
-            UnitsInfographicProfile unitsProfile = GraphicsModel.INSTANCE.getActiveProfile().getUnitsProfile();
-            Graphics2D unitGraphics = unitImage.createGraphics();
-            unitsProfile.paintUnitAttributes(unitGraphics, unit);
-            unitGraphics.dispose();
-            g2.drawImage(unitImage, pos.x + d, pos.y + d, this);
-            repaint(pos.x, pos.y, unitImage.getWidth() + d, unitImage.getHeight() + d);
+            //Paint additional unit info (Health, Stamina, Attack, Defense, etc.)
+            
+            Graphics2D unitG2 = subImage.createGraphics();
+            unitG2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            UnitsInfographicProfile unitsProfile = graphicsProfile.getUnitsProfile();
+            unitsProfile.paintUnitAttributes(unitG2, unit);
+//            unitsProfile.paintUnitAttributes(g2, unit);
+            unitG2.dispose();
+            repaint(pos.x, pos.y, unitImage.getWidth() + maxOffset, unitImage.getHeight() + maxOffset);
         }
 
     }

src/ares/application/gui/GraphicsProfile.java
@@ -111,12 +111,32 @@ public interface GraphicsProfile {
      * @return the rise of the hexagon
      */
     double getHexRise();
-    
+
     int getFontSize();
-    
+
     int getLedSize();
-    
+
     int getBarSize();
-    
+
     UnitsInfographicProfile getUnitsProfile();
+
+    /**
+     * @return the offset distance from the left and upper corners of the tile(same distance vertically and
+     * horizontally). The image will be painted at Point(X+offset, Y+offset)
+     */
+    public int getUnitImageOffset();
+
+    /**
+     * A stack of units is represented showing overlapping unit images. Each layer is shifted this number of pixels. For
+     * example, say the first unit was painted at Point(X,Y), then the next layer will start at
+     * Point(X+offset,Y+offset), and the third one at Point(X+2*offset, Y+2*offset) and so on.
+     *
+     * @return the offset distance both horizontally and vertically between unit images painted in the same tile.
+     */
+    public int getUnitStackOffset();
+
+    /**
+     * @return the maximum numbers of units to be painted in a single tile
+     */
+    int getMaxUnitsStack();
 }

commit c85d089b1e590cfc983e57df71bcd635e789928a

Fixes bug introduced in commit 2af7412 that produced an "invisible" crash when using the maximum zoom. This error was due to to a 0*0  product in UnitsLayer.

src/ares/application/gui/AresGraphicsProfile.java
@@ -11,7 +11,7 @@ import ares.platform.io.ResourcePath;
 public enum AresGraphicsProfile implements GraphicsProfile {
 
     // ResourcePath, Units (Width,Height), Terrain(W,H), Hex(Side,Offset), Unit image offset, unit stack offset, max unit stack, Font size, Led vert. size, Bar vert. size
-    SMALL(ResourcePath.GRAPHICS_SMALL, 272, 128, 216, 192, 13, 21, 3, 0, 0, 5, 2, 5) {
+    SMALL(ResourcePath.GRAPHICS_SMALL, 272, 128, 216, 192, 13, 21, 3, 0, 1, 0, 0, 0) {
         @Override
         public String getFilename(String filename) {
             return "s_" + filename;

src/ares/application/gui/board/TerrainLayer.java
@@ -2,7 +2,6 @@ package ares.application.gui.board;
 
 import ares.application.gui.GraphicsModel;
 import ares.application.gui.AbstractImageLayer;
-import ares.application.gui.GraphicsProfile;
 import ares.application.gui.providers.AresMiscGraphics;
 import ares.application.models.ScenarioModel;
 import ares.application.models.board.*;
@@ -15,9 +14,9 @@ import java.util.*;
 import javax.swing.JViewport;
 
 /**
- * Terrain image layer based on Sergio Musoles TerrainPanel
  *
  * @author Heine <heisncfr@inf.upv.es>
+ * @author Mario Gómez Martínez <margomez at dsic.upv.es>
  */
 public class TerrainLayer extends AbstractImageLayer {
 
@@ -84,5 +83,4 @@ public class TerrainLayer extends AbstractImageLayer {
         }
         repaint(pos.x, pos.y, terrainImage.getWidth(), terrainImage.getHeight());
     }
-
 }

src/ares/application/gui/command/PathSearchLayer.java
@@ -11,30 +11,31 @@ import java.util.Collection;
 import javax.swing.JViewport;
 
 /**
- *Draws the pathfinding process on a BufferedImage
- * 
+ * Draws the pathfinding process on a BufferedImage
+ *
  * @author Saúl Esteban <saesmar1@ei.upv.es>
  */
 public class PathSearchLayer extends AbstractImageLayer {
+
     private Collection<Tile> openSet;
     private Collection<Tile> closedSet;
     private Graphics2D g2;
-    
+
     public PathSearchLayer(JViewport viewport, AbstractImageLayer parentLayer) {
         super(viewport, parentLayer);
     }
-    
+
     @Override
     public void updateLayer() {
         initialize();
         g2 = globalImage.createGraphics();
-        
+
         paintOpenSet();
         paintClosedSet();
-        
+
         g2.dispose();
     }
-    
+
     /**
      * Paints complete process for the {@code openSet} and {@code closedSet} passed as argument
      *
@@ -46,19 +47,19 @@ public class PathSearchLayer extends AbstractImageLayer {
         this.closedSet = closedSet;
         updateLayer();
     }
-    
+
     private void paintOpenSet() {
         for (Tile tile : openSet) {
             paintTile(tile, TileType.OPEN_SET);
         }
     }
-    
+
     private void paintClosedSet() {
         for (Tile tile : closedSet) {
             paintTile(tile, TileType.CLOSED_SET);
         }
     }
-    
+
     private void paintTile(Tile tile, TileType type) {
         BufferedImage tileImage = GraphicsModel.INSTANCE.getActiveProvider(type.getProvider()).getImage(AresIO.ARES_IO);
         Point pos = GraphicsModel.INSTANCE.tileToPixel(tile.getCoordinates());

src/ares/application/gui/command/SelectionLayer.java
@@ -14,7 +14,7 @@ import javax.swing.JViewport;
 /**
  * Draws the movement arrows on a BufferedImage
  *
- * @author Heine <heisncfr@inf.upv.es>
+ * @author Mario Gómez Martínez <margomez at dsic.upv.es>
  */
 public class SelectionLayer extends AbstractImageLayer {
 

src/ares/application/gui/forces/UnitsLayer.java
@@ -13,9 +13,9 @@ import java.util.*;
 import javax.swing.JViewport;
 
 /**
- * Units image layer based on Sergio Musoles TerrainPanel
  *
  * @author Heine <heisncfr@inf.upv.es>
+ * @author Mario Gomez <margomez at dsic.upv.es>
  */
 public class UnitsLayer extends AbstractImageLayer {
 
@@ -24,7 +24,7 @@ public class UnitsLayer extends AbstractImageLayer {
     public UnitsLayer(JViewport viewport) {
         super(viewport);
     }
-    
+
     @Override
     public void updateLayer() {
         initialize();
@@ -78,28 +78,26 @@ public class UnitsLayer extends AbstractImageLayer {
             UnitModel unit = tile.getTopUnit();
             BufferedImage unitImage = GraphicsModel.INSTANCE.getActiveProvider(unit.getColor()).getImage(unit.getIconId(), AresIO.ARES_IO);
             GraphicsProfile graphicsProfile = GraphicsModel.INSTANCE.getActiveProfile();
-            //Num units to be painted
-            int unitsToPaint = Math.min(tile.getNumStackedUnits(), graphicsProfile.getMaxUnitsStack());
-            // Attributes are painted only on the last image
-            unitsToPaint--;
 
             // Offset from the upper left corner of the tile
             int imageOffset = graphicsProfile.getUnitImageOffset();
             pos.x += imageOffset;
             pos.y += imageOffset;
-
-            int stackOffset = graphicsProfile.getUnitStackOffset();
             // Offset from the upper left corner of the last painted unit
-            int maxOffset = unitsToPaint * stackOffset;
+            int stackOffset = graphicsProfile.getUnitStackOffset();
+            //Num units to be painted
+            int unitsToPaint = Math.min(tile.getNumStackedUnits(), graphicsProfile.getMaxUnitsStack());
+            // Max stack  offset
+            int maxStackOffset = unitsToPaint * stackOffset - stackOffset;
 
             //Paint the same top unit
-            for (int d = maxOffset; d >= 0; d -= stackOffset) {
+            for (int d = maxStackOffset; d >= 0; d -= stackOffset) {
                 g2.drawImage(unitImage, pos.x + d, pos.y + d, this);
             }
             BufferedImage subImage = globalImage.getSubimage(pos.x, pos.y, graphicsProfile.getUnitWidth(), graphicsProfile.getUnitHeight());
 
             //Paint additional unit info (Health, Stamina, Attack, Defense, etc.)
-            
+
             Graphics2D unitG2 = subImage.createGraphics();
             unitG2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 //            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -107,7 +105,7 @@ public class UnitsLayer extends AbstractImageLayer {
             unitsProfile.paintUnitAttributes(unitG2, unit);
 //            unitsProfile.paintUnitAttributes(g2, unit);
             unitG2.dispose();
-            repaint(pos.x, pos.y, unitImage.getWidth() + maxOffset, unitImage.getHeight() + maxOffset);
+            repaint(pos.x, pos.y, unitImage.getWidth() + maxStackOffset, unitImage.getHeight() + maxStackOffset);
         }
 
     }