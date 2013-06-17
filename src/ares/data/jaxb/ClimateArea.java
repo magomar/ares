8a16541ce73bc69bebd65cd8c9758ad161f0ba6b
magomar <magomar@gmail.com>
2013-06-17 16:33:26 +0200

    Add jaxb generated classes (to not depend on Netbeans jax automatic code generation)

---
+++ src/ares/data/jaxb/ClimateArea.java
@@ -0,0 +1,40 @@
+
+package ares.data.jaxb;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ClimateArea.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="ClimateArea">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="NORTHERN"/>
+ *     &lt;enumeration value="SOUTHERN"/>
+ *     &lt;enumeration value="ECUATORIAL"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "ClimateArea", namespace = "ares")
+@XmlEnum
+public enum ClimateArea {
+
+    NORTHERN,
+    SOUTHERN,
+    ECUATORIAL;
+
+    public String value() {
+        return name();
+    }
+
+    public static ClimateArea fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
---
+++ src/ares/data/jaxb/Frontage.java
@@ -0,0 +1,40 @@
+
+package ares.data.jaxb;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for Frontage.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="Frontage">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="NARROW"/>
+ *     &lt;enumeration value="NORMAL"/>
+ *     &lt;enumeration value="WIDE"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "Frontage", namespace = "ares")
+@XmlEnum
+public enum Frontage {
+
+    NARROW,
+    NORMAL,
+    WIDE;
+
+    public String value() {
+        return name();
+    }
+
+    public static Frontage fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
