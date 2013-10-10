7751dbb16be28c6f66c76457cf6b823e001812d8
magomar <magomar@gmail.com>
2013-10-10 13:10:25 +0200

    Colors of translucent button depend on the theme

--- src/ares/application/shared/gui/components/TranslucentButton.java
+++ src/ares/application/shared/gui/components/TranslucentButton.java
@@ -1,6 +1,7 @@
 package ares.application.shared.gui.components;
 
 import ares.application.shared.gui.laf.ColorPalette;
+import ares.application.shared.gui.laf.ColorUtil;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,24 +11,26 @@ import java.awt.geom.RoundRectangle2D;
  * @author Mario Gómez Martínez <magomar@gmail.com>
  */
 public class TranslucentButton extends JButton {
-    private static final Color TL = new Color(1f, 1f, 1f, .2f);
-    private static final Color BR = new Color(0f, 0f, 0f, .4f);
-    private static final Color ST = new Color(1f, 1f, 1f, .2f);
-    private static final Color SB = new Color(1f, 1f, 1f, .1f);
-    private Color ssc;
-    private Color bgc;
+    private final Color focus;
+    private final Color background;
+    private final Color select;
     private int r = 8;
 
-    public TranslucentButton(String text) {
-        super(text);
-    }
-
-    public TranslucentButton(String text, Icon icon) {
-        super(text, icon);
-    }
+//    public TranslucentButton(String text) {
+//        super(text);
+//    }
+//
+//    public TranslucentButton(String text, Icon icon) {
+//        super(text, icon);
+//    }
 
     public TranslucentButton(Action a) {
         super(a);
+        this.setForeground(UIManager.getColor("Button.background"));
+        this.setFont(getFont().deriveFont(Font.BOLD, 16));
+        focus = ColorUtil.getTranslucentColor(ColorPalette.ARMY_YELLOW, 0.5f);
+        background = ColorUtil.getTranslucentColor(UIManager.getColor("Button.foreground"), 0.5f);
+        select = ColorUtil.shade(focus, 0.5);
     }
 
     @Override
@@ -37,8 +40,8 @@ public class TranslucentButton extends JButton {
         setFocusPainted(false);
         setBorderPainted(false);
         setOpaque(false);
-        this.setForeground(ColorPalette.ARMY_YELLOW);
-        this.setFont(getFont().deriveFont(Font.BOLD, 16));
+//        this.setForeground(ColorPalette.ARMY_YELLOW);
+//        this.setFont(getFont().deriveFont(Font.BOLD, 16));
     }
 
     @Override
@@ -50,28 +53,24 @@ public class TranslucentButton extends JButton {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Shape area = new RoundRectangle2D.Float(x, y, w - 1, h - 1, r, r);
-        ssc = TL;
-        bgc = BR;
         ButtonModel buttonModel = getModel();
-        if (buttonModel.isPressed()) {
-            ssc = SB;
-            bgc = ST;
-        } else if (buttonModel.isRollover()) {
-            ssc = ST;
-            bgc = SB;
+        if (buttonModel.isArmed()) {
+            g2.setColor(select);
+        } else {
+            if (this.hasFocus() || buttonModel.isRollover()) {
+                g2.setColor(focus);
+            } else {
+                g2.setColor(background);
+            }
         }
-//        if (getModel().isArmed()) {
-//            g2.setColor(select);
-//        } else {
-//            if (this.hasFocus()) {
-//                g2.setColor(focus);
-//            } else {
-//                g2.setColor(background);
-//            }
+//        if (buttonModel.isPressed()) {
+//            ssc = SB;
+//            bgc = ST;
+//        } else if (buttonModel.isRollover()) {
+//            ssc = ST;
+//            bgc = SB;
 //        }
-        g2.setPaint(new GradientPaint(x, y, ssc, x, y + h, bgc, true));
         g2.fill(area);
-        g2.setPaint(BR);
         g2.draw(area);
         g2.dispose();
         super.paintComponent(g);
